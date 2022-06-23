2315437565f2f12d0f8346011d5c501d8ad4d422
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-06-23 22:52:35 +0300

    add

---
+++ haberler/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Makale(models.Model):
+    yazar = models.CharField(max_length=150)
+    baslik = models.CharField(max_length=120)
+    acıklama = models.CharField(max_length=200)
+    metin = models.TextField()
+    sehir = models.CharField(max_length=50)
+    yayımlanma_tarihi = models.DateField()
+    aktif = models.BooleanField(default=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    guncelleme_tarihi = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.baslik
