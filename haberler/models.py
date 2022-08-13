8762e2ea36c334ecebfc2b8525b2b63ff908cb6a
Albert Abraham <93812075+Albertabrahams@users.noreply.github.com>
2022-08-13 12:18:20 +0300

    Update models.py

--- haberler/models.py
+++ haberler/models.py
@@ -8,7 +8,7 @@ class Gazeteci(models.Model):
     def __str__(self):
         return self.isim + ' ' + self.soyisim
     
-    
+
 class Makale(models.Model):
     yazar = models.ForeignKey(Gazeteci, on_delete=models.CASCADE, related_name='makaleler')
     baslik = models.CharField(max_length=120)
